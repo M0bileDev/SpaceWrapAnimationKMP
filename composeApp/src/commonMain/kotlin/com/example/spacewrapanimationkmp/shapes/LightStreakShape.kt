32ebf8176879f6258df1920b09c7f985934b78f5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 23:57:29 +0200

    feature(shapes): add composable arguments

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/shapes/LightStreakShape.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/shapes/LightStreakShape.kt
@@ -2,10 +2,21 @@ package com.example.spacewrapanimationkmp.shapes
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LightStreakShape(
+    degrees: Float = 0f,
+    rotation: Float = 0f,
+    scale: Float = 1f,
+    offset: Float = 1f,
+    color: Color = Color.Yellow,
+    cornerShapeSize: Dp = 0.dp,
+    width: Dp = 10.dp,
+    height: Dp = 40.dp,
     modifier: Modifier = Modifier
 ) {
-
 }
+    
