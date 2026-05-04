c4fde34744d839c96684e9f16bff9a4d72e52d4e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 00:22:34 +0200

    feature(components): create component that represents space

---
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/components/Space.kt
@@ -0,0 +1,10 @@
+package com.example.spacewrapanimationkmp.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Space(
+    modifier: Modifier = Modifier
+) {
+}
