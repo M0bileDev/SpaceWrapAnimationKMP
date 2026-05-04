9d2f780547abf3c0764adc5c3498ebd19fb1cdc6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 00:32:17 +0200

    feature(components): create container

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/components/Space.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/components/Space.kt
@@ -1,5 +1,8 @@
 package com.example.spacewrapanimationkmp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,5 +14,11 @@ fun Space(
     spaceBackground: Color = Color.Black,
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(spaceBackground)
+    ) {
 
+    }
 }
