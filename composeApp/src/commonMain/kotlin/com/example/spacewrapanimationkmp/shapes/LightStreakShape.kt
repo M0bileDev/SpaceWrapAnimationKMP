316782e3e7a7c7c95971a00f6bf329bf546b7802
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 23:58:06 +0200

    feature(shapes): create container

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/shapes/LightStreakShape.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/shapes/LightStreakShape.kt
@@ -1,8 +1,14 @@
 package com.example.spacewrapanimationkmp.shapes
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -18,5 +24,19 @@ fun LightStreakShape(
     height: Dp = 40.dp,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier
+            .rotate(degrees)
+            .graphicsLayer {
+                rotationX = rotation
+                scaleX = scale
+                scaleY = scale
+                translationY = offset
+            }
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
 }
-    
