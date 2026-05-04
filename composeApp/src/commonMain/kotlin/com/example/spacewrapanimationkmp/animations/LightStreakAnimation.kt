2d07e7c08e138740761f1444852b348faaca61e6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 00:05:04 +0200

    feature(animations): add composable arguments

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/animations/LightStreakAnimation.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/animations/LightStreakAnimation.kt
@@ -3,5 +3,9 @@ package com.example.spacewrapanimationkmp.animations
 import androidx.compose.runtime.Composable
 
 @Composable
-fun LightStreakAnimation() {
+fun LightStreakAnimation(
+    scale: Float = 1f,
+    durationMillis: Int = 0,
+    delayMillis: Int = 0,
+) {
 }
