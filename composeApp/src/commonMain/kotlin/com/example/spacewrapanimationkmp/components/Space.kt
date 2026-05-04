5da3c966d1ec5c8423173370b43f2516465c3e30
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 00:30:56 +0200

    feature(components): add composable arguments

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/components/Space.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/components/Space.kt
@@ -2,9 +2,14 @@ package com.example.spacewrapanimationkmp.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.spacewrapanimationkmp.model.SpaceRepresentation
 
 @Composable
 fun Space(
+    spaceRepresentation: SpaceRepresentation,
+    spaceBackground: Color = Color.Black,
     modifier: Modifier = Modifier
 ) {
+
 }
