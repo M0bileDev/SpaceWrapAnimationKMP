51c6b4dafc6afa5e0b932f838a6bd500e54348f5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 00:06:52 +0200

    feature(animations): add argument
    Added animated transitions for values as scale and offset

--- composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/animations/LightStreakAnimation.kt
+++ composeApp/src/commonMain/kotlin/com/example/spacewrapanimationkmp/animations/LightStreakAnimation.kt
@@ -1,11 +1,39 @@
 package com.example.spacewrapanimationkmp.animations
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 
 @Composable
 fun LightStreakAnimation(
     scale: Float = 1f,
     durationMillis: Int = 0,
     delayMillis: Int = 0,
+    offset: Float = 1f
 ) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = scale,
+        animationSpec = infiniteRepeatable(
+            tween(
+                delayMillis = delayMillis,
+                durationMillis = durationMillis
+            ), RepeatMode.Restart
+        )
+    )
+    val offset by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = offset,
+        animationSpec = infiniteRepeatable(
+            tween(
+                delayMillis = delayMillis,
+                durationMillis = durationMillis
+            ), RepeatMode.Restart
+        )
+    )
 }
